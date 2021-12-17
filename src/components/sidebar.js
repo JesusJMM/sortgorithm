2565942269eca564b9809e6faced873ccb0080f4
JesusJMM <jsusmarcano.jm@gmail.com>
2021-12-17 12:57:30 -0400

    title changed, landing page link added

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -22,11 +22,12 @@ export default function SideBar(props) {
           <ModalHeader>{i.title}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text pb={4} fontWeight="bold">
+            <Text pb={4} fontWeight="medium">
               {i.info}
             </Text>
-            <Text fontWeight="bold">This are some articles that will give you more information about it.</Text>
+            <Text fontWeight="medium">This are some articles that will give you more information about it.</Text>
             {i.moreInfo.map((mi, index) => (<Text fontWeight="bold" key={index}><Link isExternal color="#0085FF" href={mi.url}>{mi.title}</Link></Text>))}
+            <Text pt={4} text="medium">Remeber check it out the{" "}<Link fontWeight="bold" color="#0085FF" href="https://jesusjmm.github.io/sortgorithm-page/">landing page</Link></Text>
           </ModalBody>
           <ModalFooter >
             <Button onClick={props.onClose} colorScheme="blue">Close</Button>
